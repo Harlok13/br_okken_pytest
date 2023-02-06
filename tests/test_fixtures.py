9addebdb9f3fb97af15c2ca40b7de25537c58561
Harlok <holmik90@yandex.ru>
2023-02-06 21:57:25 +0300

    upd

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,11 +2,11 @@ import pytest
 
 
 @pytest.fixture()
-def a_tuple():
-    """Вернуть что-нибудь более интересное"""
-    return (1, 'foo', None, {'bar': 23})
+def some_data():
+    """Return answer to ultimate question."""
+    return 42
 
 
-def test_a_tuple(a_tuple):
-    """Demo the a_tuple fixture."""
-    assert a_tuple[3]['bar'] == 32
+def test_some_data(some_data):
+    """Use fixture return value in a test."""
+    assert some_data == 42
